1c62d33774b3a5523a94dcc61ea273246ec27fd1
miruna <avrammiruna2001@yahoo.com>
2023-05-04 21:41:08 +0300

    monitoring user controller fixes

--- src/main/java/ro/unibuc/hello/controller/UserController.java
+++ src/main/java/ro/unibuc/hello/controller/UserController.java
@@ -1,5 +1,8 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 ;
@@ -15,6 +18,8 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    MeterRegistry metricsRegistry;
 
     @GetMapping("/user/getAll")
     @ResponseBody
